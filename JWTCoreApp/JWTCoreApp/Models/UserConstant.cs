2134583ba7e55cb29f1098f704985b8ae58e319c
Kailash <programmer.joshi@gmail.com>
2023-08-15 20:07:45 -0700

    JWTTokenAuthenticationCore

---
+++ JWTCoreApp/JWTCoreApp/Models/UserConstant.cs
@@ -0,0 +1,11 @@
+﻿namespace JWTCoreApp.Models
+{
+    public class UserConstant
+    {
+        // We are not taking data from data base so we get data from constant
+        public static List<UserModel> Users = new()
+            {
+                    new UserModel(){ Username="joshi",Password="admin123",Role="Admin"}
+            };
+    }
+}
